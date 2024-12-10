ec7bf06d9c3e89ae1b7024f4ff498f5a1692f418
Sami Uysal <samiuysal.business@gmail.com>
2024-12-10 23:21:49 +0300

    Bugs fixed.

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,9 +1,16 @@
-from scapy.all import sniff, conf
+from scapy.all import sniff
 from feature_extractor import extract_features
 import json
 
+def classify_packet(packet):
+
+    return 1 if "malicious_condition" in str(packet) else 0 
+
 def save_packet(packet):
     features = extract_features(packet)
+
+    features['label'] = classify_packet(packet)
+
     with open("packets.json", "a") as f:
         f.write(json.dumps(features) + "\n")
 
